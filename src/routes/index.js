e36e38003f9b4ff3221342813c38ae44719d362e
felipeNasci <dfnascimento@live.com>
2021-10-31 19:59:40 -0300

    feat: add swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,9 @@
 const routes = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../resourses/configs/swagger.json');
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 const signUp = require('./sign-up');
 
