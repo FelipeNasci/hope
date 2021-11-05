0dc55f187bd2c6b7294fc1bd41f35af6b39af41e
Diego Felipe <36546304+FelipeNasci@users.noreply.github.com>
2021-11-05 08:58:34 -0300

    Revert "Feature/create communities (#1)" (#2)
    This reverts commit d982f3a86402f6ef95210a4290949a1f3e87b972.

--- src/resources/configs/deploy.js
+++ src/resources/configs/deploy.js
@@ -1,4 +1,3 @@
 module.exports = {
-    port: process.env.PORT || 3005,
-    url: process.env.APP_URL
+    port: process.env.PORT || 3005
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,13 @@
 const routes = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../resources/configs/swagger');
+const swaggerDocument = require('../resources/configs/swagger.json');
 
 const account = require('./account');
-const auth = require('./authenticated-route');
-const community = require('./community');
 
 routes.use('/api-docs', swaggerUi.serve);
 routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-routes.use(account);
-routes.use(auth);
-routes.use(community);
+
+routes.use(account)
 
 module.exports = routes;
